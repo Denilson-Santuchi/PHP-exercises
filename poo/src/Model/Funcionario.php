55d39de9daa3c82efe4496660878c0db35909c73
Denilson <denilsonsantuchi@hotmail.com>
2023-02-02 16:37:35 -0300

    feat: bonificacao

--- poo/src/Model/Funcionario.php
+++ poo/src/Model/Funcionario.php
@@ -5,15 +5,32 @@ namespace Alura\Banco\Model;
 class Funcionario extends Pessoa
 {
     private string $cargo;
+    private float $salario;
 
-    public function __construct(Cpf $cpf, string $nome, string $cargo)
+    public function __construct(Cpf $cpf, string $nome, string $cargo, string $salario)
     {
         parent::__construct($nome, $cpf);
+        $this->salario = $salario;
         $this->cargo = $cargo;
     }
 
     public function recuperacargo()
     {
         return $this->cargo;
+    }
+
+    public function alteraNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function recuperaSalario(): float
+    {
+        return $this->salario;
+    }
+
+    public function calculaBonificacao(): float
+    {
+        return $this->salario * 0.1;
     }
 }
---
+++ poo/src/Service/ControladorDeBonificacoes.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Alura\Banco\Service;
+
+use Alura\Banco\Model\Funcionario;
+
+class ControladorDeBonificacoes
+{
+    private float $totalDeBonificacoes = 0;
+
+    public function adicionaBonificacoesDe(Funcionario $funcionario)
+    {
+        $this->totalDeBonificacoes += $funcionario->calculaBonificacao();
+    }
+
+    public function recuperaBonificacoes(): float
+    {
+        return $this->totalDeBonificacoes;
+    }
+}
