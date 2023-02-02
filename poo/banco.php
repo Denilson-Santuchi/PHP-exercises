8ac4e485ef901ec013ea289c02b80399750de98b
Denilson <denilsonsantuchi@hotmail.com>
2023-02-02 09:20:12 -0300

    feat: add getters and setters

--- poo/banco.php
+++ poo/banco.php
@@ -3,6 +3,12 @@
 require_once 'src/Conta.php';
 
 $primeiraConta = new Conta();
-$segundaConta = new Conta();
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300);
+$primeiraConta->defineCpfDoTitular('123.456.789-10');
 
-?>
+
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+echo $primeiraConta->recuperaCpfDoTitular() . PHP_EOL;
+
+$segundaConta = new Conta();
--- poo/src/Conta.php
+++ poo/src/Conta.php
@@ -6,7 +6,7 @@ class Conta
     private string $nomeDoTitular;
     private float $saldo = 0;
 
-    public function sacar(float $valorASacar): void
+    public function saca(float $valorASacar): void
     {
         if ($valorASacar > $this->saldo) {
             echo 'saldo indisponivel';
@@ -16,7 +16,7 @@ class Conta
         $this->saldo -= $valorASacar;
     }
 
-    public function depositar(float $valorADepositar): void
+    public function deposita(float $valorADepositar): void
     {
         if ($valorADepositar <= 0) {
             echo 'operação inválida!';
@@ -26,16 +26,39 @@ class Conta
         $this->saldo += $valorADepositar;
     }
 
-    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
     {
         if ($valorATransferir > $this->saldo) {
             echo 'saldo indisponivel';
             return;
         }
 
-        $this->sacar($valorATransferir);
-        $contaDestino->depositar($valorATransferir);
+        $this->saca($valorATransferir);
+        $contaDestino->deposita($valorATransferir);
     }
 
-    
+    public function recuperaSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function recuperaCpfDoTitular(): string
+    {
+        return $this->cpfDoTitular;
+    }
+
+    public function recuperaNomeDoTitular(): string
+    {
+        return $this->nomeDoTitular;
+    }
+
+    public function defineCpfDoTitular(string $cpf): void
+    {
+        $this->cpfDoTitular = $cpf;
+    }
+
+    public function defineNomeDoTitular(string $nome): void
+    {
+        $this->nomeDoTitular = $nome;
+    }
 }
