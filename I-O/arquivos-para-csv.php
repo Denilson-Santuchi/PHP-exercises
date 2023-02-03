3df02f6c4e300a908687a7c91a072b6928839115
Denilson <denilsonsantuchi@hotmail.com>
2023-02-03 11:11:18 -0300

    feat: ENCODING

--- I-O/arquivos-para-csv.php
+++ I-O/arquivos-para-csv.php
@@ -6,13 +6,13 @@ $lista2 = file('listas-tests.txt');
 $arquivoCsv = fopen('cursos.csv', 'w');
 
 foreach ($lista as $value) {
-    $line = [trim($value), 'Sim'];
+    $line = [trim(mb_convert_encoding($value, 'Windows-1252', 'UTF-8')), 'Sim'];
 
     fputcsv($arquivoCsv, $line, ';');
 }
 
 foreach ($lista2 as $value) {
-    $line = [trim($value), 'Não'];
+    $line = [trim(mb_convert_encoding($value, 'Windows-1252', 'UTF-8')), 'Não'];
 
     fputcsv($arquivoCsv, $line, ';');
 }
