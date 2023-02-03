bd37b8708c467cc0f4e3f9375259baefdfdce4eb
Denilson <denilsonsantuchi@hotmail.com>
2023-02-03 08:46:57 -0300

    feat: forma verbosa de escrever no arquivo

--- I-O/escritor-tests.php
+++ I-O/escritor-tests.php
@@ -1,9 +1,5 @@
 <?php
 
-$archive = fopen('lista-tests.txt', 'a');
-
 $line = "\nwill add in arquive";
 
-fwrite($archive, $line);
-
-fclose($archive);
+file_put_contents('lista-tests.txt', $line, FILE_APPEND);
