2f5dfce524f14e6267704d73a51921fda0df8899
Denilson <denilsonsantuchi@hotmail.com>
2023-02-09 09:16:22 -0300

    feat: escrevendo testes

--- phpunit-leilao/src/Service/Avaliador.php
+++ phpunit-leilao/src/Service/Avaliador.php
@@ -10,9 +10,12 @@ class Avaliador
 
     public function avalia(Leilao $leilao): void
     {
-        $lances = $leilao->getLances();
-        $ultimoLance = $lances[count($lances) - 1];
-        $this->maiorValor = $ultimoLance->getValor();
+        $this->maiorValor = 0;
+        foreach ($leilao->getLances() as $lance) {
+            if ($lance->getValor() > $this->maiorValor) {
+                $this->maiorValor = $lance->getValor();
+            }
+        }
     }
 
     public function getMaiorValor(): float
---
+++ phpunit-leilao/tests/Services/AvaliadorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Alura\Leilao\Tests\Service;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+use PHPUnit\Framework\TestCase;
+
+class AvaliadorTest extends TestCase
+{
+    public function testMaiorValorEmOrdemCrescente()
+    {
+        $leilao = new Leilao('Fiat 147 OKM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('Joao');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+        $this->assertEquals(2500, $maiorValor);
+    }
+
+    public function testMaiorValorEmOrdemDecrescente()
+    {
+        $leilao = new Leilao('Fiat 147 OKM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('Joao');
+
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+        $this->assertEquals(2500, $maiorValor);
+    }
+}
