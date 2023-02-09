14e414c4f6ee02f7be2fd3b276cc45bdfa26b43e
Denilson <denilsonsantuchi@hotmail.com>
2023-02-09 10:04:09 -0300

    feat: testa e implementa maiores lances

--- phpunit-leilao/src/Service/Avaliador.php
+++ phpunit-leilao/src/Service/Avaliador.php
@@ -2,12 +2,14 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private float $maiorValor;
     private float $menorValor;
+    private array $maioresLances;
 
     public function avalia(Leilao $leilao): void
     {
@@ -22,6 +24,12 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+
+        $lances = $leilao->getLances();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
@@ -32,5 +40,11 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    /** @return Lance[] */
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
--- phpunit-leilao/tests/Services/AvaliadorTest.php
+++ phpunit-leilao/tests/Services/AvaliadorTest.php
@@ -77,4 +77,27 @@ class AvaliadorTest extends TestCase
         $menorValor = $leiloeiro->getMenorValor();
         $this->assertEquals(2000, $menorValor);
     }
+
+    public function test3MaioresLances()
+    {
+        $leilao = new Leilao('Gol Bolinha 0KM');
+        $joao = new Usuario('João');
+        $denilson = new Usuario('Denilson');
+        $maria = new Usuario('Maria');
+        $vitor = new Usuario('Vitor');
+
+        $leilao->recebeLance(new Lance($joao, 1500));
+        $leilao->recebeLance(new Lance($denilson, 3000));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($vitor, 2500));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        $maiores = $leiloeiro->getMaioresLances();
+        $this->assertCount(3, $maiores);
+        $this->assertEquals(3000, $maiores[0]->getValor());
+        $this->assertEquals(2500, $maiores[1]->getValor());
+        $this->assertEquals(2000, $maiores[2]->getValor());
+    }
 }
