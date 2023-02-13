994ece83b704c99a67f37155ec8be57f8b2725d6
Denilson <denilsonsantuchi@hotmail.com>
2023-02-13 09:11:52 -0300

    feat: Series Controller

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    echo 'Wello word!';
-});
+Route::get('/series', [SeriesController::class, 'index']);
