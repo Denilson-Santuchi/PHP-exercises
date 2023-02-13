af6d3884a701c85a3a4d00b064ad162880aa6ac7
Denilson <denilsonsantuchi@hotmail.com>
2023-02-13 09:44:49 -0300

    refactor: adpatando aquivo de view usando o blade

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -14,6 +14,6 @@ class SeriesController extends Controller
             'The last of us'
         ];
 
-        return view('listar-series', compact('series'));
+        return view('listar-series')->with(compact('series'));
     }
 }
